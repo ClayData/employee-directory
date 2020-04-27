0b7d7cd65b7790fd46ef423022055ed6c9b7b904
ClayData <claytonortiz96@gmail.com>
2020-04-27 15:27:01 -0500

    MVP, searches by Job only

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Table from "./components/Table";
+import { SearchBox } from "./components/SearchBox";
+import rows from './components/employees.json'
 
-function App() {
-  return (
-    
-    <Table />
-  )
+
+class App extends React.Component {
+
+  state = {
+    searchField: '',
+    rows: rows
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value})
+  }
+
+  render(){  
+
+    const{ rows, searchField } = this.state;
+    const filteredRows = rows.filter(row => row.job.toLowerCase().includes(searchField.toLowerCase()))
+
+    return (
+    <div className="conatainer">
+    <SearchBox 
+    handleChange={this.handleChange}
+    />
+    <Table 
+    rows={filteredRows}
+    />
+    </div>
+  )}
 }
 
 export default App;
--- employee-directory/src/components/Table.js
+++ employee-directory/src/components/Table.js
@@ -2,13 +2,7 @@ import React from 'react';
 import Row from './Row';
 import data from './employees.json'
 
-class Table extends React.Component{
-
-    state = {
-        data
-    }
-
-    render() {
+function Table (props){    
         return(
             <table>
                 <tr>
@@ -17,19 +11,19 @@ class Table extends React.Component{
                     <th>Age</th>
                     <th>Job</th>
                 </tr>
-                {this.state.data.map((data) => (
+                {props.rows.map((row) => (
                     <Row 
-                    employee={data.employee}
-                    gender={data.gender}
-                    key={data.id}
-                    age={data.age}
-                    job={data.job}
+                    employee={row.employee}
+                    gender={row.gender}
+                    key={row.id}
+                    age={row.age}
+                    job={row.job}
                     />
                 ))
                 }
             </table>
         )
-    }
+    
 }
 
 export default Table;
